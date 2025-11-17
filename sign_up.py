01cec2e70dd5624f6539fa0c51ccf94e27a2426c
huanzhic <huanzhic@oregonstate.edu>
2025-11-17 12:58:15 -0800

    update comment in sign_up

--- sign_up.py
+++ sign_up.py
@@ -1,43 +1,51 @@
 import re
 
-def _is_valid_username(username: str) -> bool:
-    pattern = r'^[A-Za-z0-9_]+$'
-    return re.match(pattern, username) is not None
-
 
 def sign_up(username, password, data):
+    # Prevent empty input
     if not username or not password:
         return "NA"
 
+    # Check whether it contains any invalid characters
     if not _is_valid_username(username):
         return "NA"
 
     users = data.get("users", [])
 
+    # Check whether the username already exists
     for user in users:
         if user.get("username") == username:
             return "NA"
 
+    # Generate a new user ID
     new_id = _generate_userid(users)
 
+    # Create a new user
     new_user = {
         "id": new_id,
         "username": username,
         "password": password
     }
 
+    # Add new user to the list and send to data
     users.append(new_user)
     data["users"] = users
 
     return new_id
 
+def _is_valid_username(username: str) -> bool:
+    # The password rule is: it can only contain letters, digits, and underscores
+    pattern = r'^[A-Za-z0-9_]+$'
+    return re.match(pattern, username) is not None
 
 def _generate_userid(users):
+    # No users, return user_001
     if not users:
         return "user_001"
 
     used_numbers = set()
 
+    # Generate the smallest unused user_xxx based on the existing list of user IDs.
     for user in users:
         user_id = user.get("id", "")
         parts = user_id.split("_")
