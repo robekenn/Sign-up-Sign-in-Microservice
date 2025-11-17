dff0cb88b52b176c8e5dc30e65bc8f08b430aee8
huanzhic <huanzhic@oregonstate.edu>
2025-11-17 12:37:42 -0800

    Create sign_in.py

---
+++ sign_in.py
@@ -0,0 +1,12 @@
+def sign_in(username, password, data):
+    # Prevent empty input
+    if not username or not password:
+        return "NA"
+
+    # Check database
+    for user in data.get("users", []):
+        if user.get("username") == username and user.get(password) == password:
+            return user.get("id")
+    
+    # Not match
+    return "NA"
